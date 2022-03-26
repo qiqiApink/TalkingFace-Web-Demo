40fdbc2af8edfe3e4302a9dab019befd8f4ac582
qiqiApink <1165394984@qq.com>
2022-03-26 10:52:53 +0800

    add bootsatrp-nav

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,25 @@
 from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import *
 import os
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+nav=Nav()
+nav.register_element(
+    'top', Navbar(
+        'FaceProject',
+        View('Home', 'index'),
+        Subgroup(
+            'TalkingFace',
+            View('Wav2Lip', 'wav2lip_upload'),
+            Separator(),
+            View('Test', 'test')
+        )
+    )
+)
+nav.init_app(app)
 UPLOAD_FOLDER = 'uploads'
 app.config['SECRET_KEY'] = 'taeyeon'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
