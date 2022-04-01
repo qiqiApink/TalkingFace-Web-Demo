fc63f235b968d0a93c3d38424e098aa37eb39ccd
ImmortalSdm <1245046380@qq.com>
2022-04-02 02:19:26 +0800

    支持视频预缓存

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from app import app
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port='23333')
+    app.run(host="0.0.0.0", port='33333')
+'''
+ssh -L 36667:127.0.0.1:33333 -p 9150 nfymzk@202.38.69.85
+'''
